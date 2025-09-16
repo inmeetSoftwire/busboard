ce3ef35a3fad6ba69da9e5279175890b067a1e27
Inmeet Khurana <Inmeet.Khurana@softwire.com>
2025-09-16 17:23:35 +0100

    Use correct way of checking if object is empty before rendering

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   function sortArrivalsAndUpdateRecord(stopId: string, arrivals: Arrival[]) {
     const sorted = arrivals.sort((a, b) => a.timeToStation - b.timeToStation).slice(0, 5);
     setArrivalsByStopId(prev => ({ ...(prev ?? {}), [stopId]: sorted }));
-}
+  }
 
   function formatArrivalTime(seconds: number) {
     const minutes = Math.round(seconds / 60);
@@ -55,7 +55,7 @@ function App() {
         </div>
 
         <div className="w-full max-w-xl bg-white shadow-md rounded-lg p-4">
-          {arrivalsByStopId.keys.length > 0 ? (
+          {Object.keys(arrivalsByStopId).length > 0 ? (
             <div className="whitespace-pre-wrap text-gray-700 text-sm">
               {stopPoints.map((sp, stopIndex) => (
                 <div key={stopIndex} className="mb-4">
