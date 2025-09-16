1485eee98fa706465f455341cd07d657ef8f4a2f
Inmeet Khurana <Inmeet.Khurana@softwire.com>
2025-09-16 11:36:56 +0100

    Add API key to repo. Currently not working as intended

--- backend/fetchArrivals.ts
+++ backend/fetchArrivals.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import type { Arrival } from './Arrival';
 
-
 async function fetchArrivals(stopId: string) {
-    const API_URL = `https://api.tfl.gov.uk/StopPoint/${stopId}/Arrivals`;
+    const TFL_API_KEY = import.meta.env.TFL_API_KEY;
+    console.log(TFL_API_KEY)
+    const API_URL = `https://api.tfl.gov.uk/StopPoint/${stopId}/Arrivals?app_key=${TFL_API_KEY}`;
     try {
         const response = await axios.get(API_URL);
         return response.data as Arrival[];
