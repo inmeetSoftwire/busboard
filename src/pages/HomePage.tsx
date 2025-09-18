c81115c2b5972364d0378e2ef55c037db7a153e8
Inmeet Khurana <Inmeet.Khurana@softwire.com>
2025-09-18 16:26:13 +0100

    Add simple HomePage

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to the Bus Board Application</h1>
+      <p>Use the navigation to find bus stops and arrival times.</p>
+    </div>
+  );
+}
