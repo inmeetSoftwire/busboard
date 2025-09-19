bfed16288d4227c87ab08da5d798cbcec07572fa
Inmeet Khurana <Inmeet.Khurana@softwire.com>
2025-09-19 14:08:25 +0100

    Change text style on home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,8 +2,8 @@
 function HomePage() {
   return (
     <main className='text-center pt-10 flex-1'>
-      <h1 className="text-3xl">Welcome to <strong className='text-cyan-700'>Inmeet's BusBoard </strong></h1>
-      <p>Use the navigation to find arrival times and learn more about the history of buses.</p>
+      <h1 className="text-3xl">Welcome to <strong className="bg-gradient-to-r bg-clip-text text-transparent from-cyan-700 to-black">Inmeet's BusBoard </strong></h1>
+      <p className="text-lg">Use the navigation to find arrival times and learn more about the history of buses.</p>
     </main>
   );
 }
