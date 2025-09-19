21fc5f2a46d66e214937a996f3926f9bd69ab955
Inmeet Khurana <Inmeet.Khurana@softwire.com>
2025-09-19 11:34:57 +0100

    Use base url and rename constant urls generated from parameters in tflService

--- backend/tflService.ts
+++ backend/tflService.ts
@@ -3,11 +3,12 @@ import type { Arrival } from './types/Arrival';
 import type { Coordinate } from './types/Coordinate';
 import type { StopPoint } from './types/StopPoint';
 const api_key = import.meta.env.VITE_TFL_API_KEY;
+const BASE_URL = 'https://api.tfl.gov.uk';
 
 export async function fetchArrivals(stopId: string) : Promise<Arrival[] | null> {
-    const API_URL = `https://api.tfl.gov.uk/StopPoint/${stopId}/Arrivals?app_key=${api_key}`;
+    const arrivalsUrl = `${BASE_URL}/StopPoint/${stopId}/Arrivals?app_key=${api_key}`;
     try {
-        const response = await axios.get<Arrival[]>(API_URL);
+        const response = await axios.get<Arrival[]>(arrivalsUrl);
         const data = response.data;
         const sortedAndSlicedData = data?.sort((a, b) => a.timeToStation - b.timeToStation).slice(0, 5);
         return sortedAndSlicedData;
@@ -19,9 +20,9 @@ export async function fetchArrivals(stopId: string) : Promise<Arrival[] | null> 
 
 export async function getStopPointsFromCoordinate(coordinate: Coordinate) : Promise<StopPoint[] | null> {
     const { latitude: lat, longitude: long } = coordinate;
-    const API_URL = `https://api.tfl.gov.uk/StopPoint/?lat=${lat}&lon=${long}&stopTypes=NaptanPublicBusCoachTram&app_key=${api_key}`;
+    const stopPointsUrl = `${BASE_URL}/StopPoint/?lat=${lat}&lon=${long}&stopTypes=NaptanPublicBusCoachTram&app_key=${api_key}`;
     try {
-        const response = await axios.get<{stopPoints: StopPoint[]}>(API_URL);
+        const response = await axios.get<{stopPoints: StopPoint[]}>(stopPointsUrl);
         const stopPoints = response.data.stopPoints;
         return stopPoints;
     } catch (error) {
