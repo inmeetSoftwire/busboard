66b63e9a0c2ac45bf532b755e369fd6c8927c595
Inmeet Khurana <Inmeet.Khurana@softwire.com>
2025-09-22 09:45:10 +0100

    Add file for map component

---
+++ src/components/MapContainer.tsx
@@ -0,0 +1,9 @@
+
+
+export default function MapContainer() {
+  return (
+    <div className="flex items-center space-x-3 w-fit max-w-xl bg-white shadow-md rounded-lg p-4 mb-4">
+
+    </div>
+  );
+}
